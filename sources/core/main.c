362a0f304aef51458e3de2bf06e6ac2fdb1253f1
Yoel RIDGWAY-LOPEZ <yoelridgway@gmail.com>
2023-03-21 23:19:05 +0100

    fixed writing by clearing the display each time

--- sources/core/main.c
+++ sources/core/main.c
@@ -6,7 +6,7 @@
 /*   By: yridgway <yridgway@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/05 21:18:58 by ana               #+#    #+#             */
-/*   Updated: 2023/03/20 20:05:53 by yridgway         ###   ########.fr       */
+/*   Updated: 2023/03/21 23:18:38 by yridgway         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,8 +41,44 @@ char	*ft_get_type_str(int type)
 	return ("UNKNOWN");
 }
 
+char	*ft_strcombine(char **arr)
+{
+	char	*str;
+	int		i;
+	int		j;
+
+	i = 0;
+	j = 0;
+	while (arr[i])
+	{
+		while (arr[i][j])
+			j++;
+		i++;
+	}
+	str = malloc(sizeof(char) * (j + 1));
+	str[j] = '\0';
+	i = 0;
+	j = 0;
+	while (arr[i])
+	{
+		while (arr[i][j])
+		{
+			str[j] = arr[i][j];
+			j++;
+		}
+		i++;
+	}
+	return (str);
+}
+
 void	display_strings(t_mrt *mrt)
 {
+	char *str1;
+	char *str2;
+
+	// mlx_destroy_image(mrt->mlx, mrt->img);
+	// mrt->img = mlx_new_image(mrt->mlx, IX, IY);
+	mlx_clear_window(mrt->mlx, mrt->win);
 	mlx_string_put(mrt->mlx, mrt->win, 10, 20, 0xFFFFFF, "MiniRT");
 	mlx_string_put(mrt->mlx, mrt->win, 10, 40, 0xFFFFFF, "Controls:");
 	mlx_string_put(mrt->mlx, mrt->win, 10, 60, 0xFFFFFF, "WASDQE - move");
@@ -51,6 +87,7 @@ void	display_strings(t_mrt *mrt)
 	mlx_string_put(mrt->mlx, mrt->win, 10, 120, 0xFFFFFF, "ESC - exit");
 	mlx_string_put(mrt->mlx, mrt->win, 10, 200, 0xFFFFFF, ft_strjoin("OBJECT: ", ft_get_type_str(mrt->curr_obj.type)));
 	// mlx_string_replace(mrt->mlx, mrt->win, 10, 220, 0xFFFFFF, ft_strjoin("INDEX: ", ft_itoa(mrt->curr_obj.index)));
+	printf("str: %s\n", ft_strcombine(arr));
 }
 
 void	render_scene(t_mrt *mrt)
