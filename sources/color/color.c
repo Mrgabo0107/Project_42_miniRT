408f76e4e3dafe96c5dea388a3a9e1c48b0a7336
yoel <yridgway@student.42.fr>
2023-03-28 14:15:12 +0200

    fixed mirrors to not get randomly brighter when multiple lights are added;

--- sources/color/color.c
+++ sources/color/color.c
@@ -6,7 +6,7 @@
 /*   By: yoel <yoel@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/01 01:47:46 by gamoreno          #+#    #+#             */
-/*   Updated: 2023/03/28 01:47:38 by yoel             ###   ########.fr       */
+/*   Updated: 2023/03/28 14:14:38 by yoel             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,11 +35,11 @@ t_rgb	get_radiance(t_mrt *mrt, t_inter *ctr, t_vec dir, t_light light)
 {
 	t_vec	to_light;
 	t_inter	linter;
-	t_rgb	reflection;
+	// t_rgb	reflection;
 	t_rgb	diffuse;
 	t_rgb	specular;
 
-	reflection = ft_make_rgb(0, 0, 0);
+	// reflection = ft_make_rgb(0, 0, 0);
 	diffuse = ft_make_rgb(0, 0, 0);
 	specular = ft_make_rgb(0, 0, 0);
 	to_light = vec_rest(light.pos, ctr->inter_coor);
@@ -51,10 +51,10 @@ t_rgb	get_radiance(t_mrt *mrt, t_inter *ctr, t_vec dir, t_light light)
 		if (ctr->option.specular > 0)
 			specular = get_specular(ctr, mrt->cam.pos, to_light, light);
 	}
-	reflection = mult_color(reflection, ctr->option.mirror);
+	// reflection = mult_color(reflection, ctr->option.mirror);
 	diffuse = mult_color(diffuse, 1 - ctr->option.mirror);
 	specular = mult_color(specular, 1 - ctr->option.mirror);
-	return (add_color(add_color(reflection, diffuse), specular));
+	return (add_color(diffuse, specular));
 }
 
 t_rgb	get_object_color(t_mrt *mrt, t_inter *ctr, t_vec dir, t_rgb color)
@@ -63,11 +63,13 @@ t_rgb	get_object_color(t_mrt *mrt, t_inter *ctr, t_vec dir, t_rgb color)
 	t_rgb	reflection;
 
 	i = -1;
+	reflection = ft_make_rgb(0, 0, 0);
 	while (++i < mrt->obj_count[LIGHT])
 		color = add_color(color, \
 		get_radiance(mrt, ctr, dir, mrt->light[i]));
 	if (ctr->option.mirror > 0 && mrt->bounce < 40)
-		reflection = get_reflection(mrt, ctr, dir);
+		reflection = mult_color(get_reflection(mrt, ctr, dir), \
+		ctr->option.mirror);
 	color = add_color(color, reflection);
 	return (add_color(color, get_ambient(ctr->color, mrt->amblight, 1)));
 }
