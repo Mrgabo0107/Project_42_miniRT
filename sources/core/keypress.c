3dca3ae7657ecac1b05eeeca0992924869e37f21
Yoel RIDGWAY-LOPEZ <yoelridgway@gmail.com>
2023-03-30 17:50:24 +0200

    fixed some norminette

--- sources/core/keypress.c
+++ sources/core/keypress.c
@@ -6,7 +6,7 @@
 /*   By: yridgway <yridgway@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/05 21:25:02 by gamoreno          #+#    #+#             */
-/*   Updated: 2023/03/29 23:45:05 by yridgway         ###   ########.fr       */
+/*   Updated: 2023/03/30 17:47:51 by yridgway         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,39 +15,39 @@
 void	ft_move_cam(t_mrt *mrt, int key)
 {
 	if (key == W)
-		mrt->cam.pos = vec_sum(mrt->cam.pos,
-							   scal_vec(0.5, mrt->cam.screen_base.n3));
+		mrt->cam.pos = vec_sum(mrt->cam.pos, \
+		scal_vec(0.5, mrt->cam.screen_base.n3));
 	if (key == S)
-		mrt->cam.pos = vec_sum(mrt->cam.pos,
-							   scal_vec(-0.5, mrt->cam.screen_base.n3));
+		mrt->cam.pos = vec_sum(mrt->cam.pos, \
+		scal_vec(-0.5, mrt->cam.screen_base.n3));
 	if (key == A)
-		mrt->cam.pos = vec_sum(mrt->cam.pos,
-							   scal_vec(-0.5, mrt->cam.screen_base.n1));
+		mrt->cam.pos = vec_sum(mrt->cam.pos, \
+		scal_vec(-0.5, mrt->cam.screen_base.n1));
 	if (key == D)
-		mrt->cam.pos = vec_sum(mrt->cam.pos,
-							   scal_vec(0.5, mrt->cam.screen_base.n1));
+		mrt->cam.pos = vec_sum(mrt->cam.pos, \
+		scal_vec(0.5, mrt->cam.screen_base.n1));
 	if (key == Q)
-		mrt->cam.pos = vec_sum(mrt->cam.pos,
-							   scal_vec(0.5, mrt->cam.screen_base.n2));
+		mrt->cam.pos = vec_sum(mrt->cam.pos, \
+		scal_vec(0.5, mrt->cam.screen_base.n2));
 	if (key == E)
-		mrt->cam.pos = vec_sum(mrt->cam.pos,
-							   scal_vec(-0.5, mrt->cam.screen_base.n2));
+		mrt->cam.pos = vec_sum(mrt->cam.pos, \
+		scal_vec(-0.5, mrt->cam.screen_base.n2));
 }
 
 void	ft_change_cam_dir(t_mrt *mrt, int key)
 {
 	if (key == UP)
-		mrt->cam.dir = vec_sum(mrt->cam.dir,
-							   scal_vec(-0.2, mrt->cam.screen_base.n2));
+		mrt->cam.dir = vec_sum(mrt->cam.dir, \
+		scal_vec(-0.2, mrt->cam.screen_base.n2));
 	if (key == DOWN)
-		mrt->cam.dir = vec_sum(mrt->cam.dir,
-							   scal_vec(0.2, mrt->cam.screen_base.n2));
+		mrt->cam.dir = vec_sum(mrt->cam.dir, \
+		scal_vec(0.2, mrt->cam.screen_base.n2));
 	if (key == LEFT)
-		mrt->cam.dir = vec_sum(mrt->cam.dir,
-							   scal_vec(-0.2, mrt->cam.screen_base.n1));
+		mrt->cam.dir = vec_sum(mrt->cam.dir, \
+		scal_vec(-0.2, mrt->cam.screen_base.n1));
 	if (key == RIGHT)
-		mrt->cam.dir = vec_sum(mrt->cam.dir,
-							   scal_vec(0.2, mrt->cam.screen_base.n1));
+		mrt->cam.dir = vec_sum(mrt->cam.dir, \
+		scal_vec(0.2, mrt->cam.screen_base.n1));
 }
 
 void	define_curr_obj(t_mrt *mrt, int type, int index)
@@ -70,7 +70,6 @@ void	chess_ctr(t_mrt *mrt, int key)
 	{
 		if (key == PLUS)
 			mrt->sphere[mrt->curr_obj.index].option.chess_ctrl++;
-
 		if (key == MINUS \
 		&& mrt->sphere[mrt->curr_obj.index].option.chess_ctrl > 0)
 			mrt->sphere[mrt->curr_obj.index].option.chess_ctrl--;
@@ -91,9 +90,6 @@ void	light_ctr(t_mrt *mrt, int key)
 		mrt->light[mrt->curr_obj.index].ratio += 0.1;
 	if (key == MINUS && mrt->light[mrt->curr_obj.index].ratio > 0.0)
 		mrt->light[mrt->curr_obj.index].ratio -= 0.1;
-	if (key == L)
-		define_curr_obj(mrt, LIGHT, \
-		(mrt->curr_obj.index + 1) % mrt->obj_count[LIGHT]);
 }
 
 int	key_press(int key, t_mrt *mrt)
@@ -120,8 +116,9 @@ int	key_press(int key, t_mrt *mrt)
 		define_curr_obj(mrt, CAMERA, 0);
 	if (mrt->curr_obj.type == LIGHT)
 		light_ctr(mrt, key);
-	else if (key == L)
-		define_curr_obj(mrt, LIGHT, 0);
+	if (key == L)
+		define_curr_obj(mrt, LIGHT, \
+		(mrt->curr_obj.index + 1) % mrt->obj_count[LIGHT]);
 	ft_move_cam(mrt, key);
 	ft_change_cam_dir(mrt, key);
 	normalize(mrt->cam.dir);
