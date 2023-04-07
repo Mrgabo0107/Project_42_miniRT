cc570596d0d324693a0eae4f840471e5f161a4fb
Gabriel Moreno <gdmorenor@unal.edu.co>
2023-04-07 14:21:45 +0200

    bump cone finished

--- sources/cone/cone_utils3.c
+++ sources/cone/cone_utils3.c
@@ -6,7 +6,7 @@
 /*   By: gamoreno <gamoreno@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/05 07:27:52 by gamoreno          #+#    #+#             */
-/*   Updated: 2023/04/07 12:57:26 by gamoreno         ###   ########.fr       */
+/*   Updated: 2023/04/07 14:20:00 by gamoreno         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,12 +33,57 @@ t_c_bump_val values)
 	return (ret);
 }
 
+double	get_cone_ang_resol(t_mrt *mrt, t_inter inter, double r_c, t_vec cyl_c)
+{
+	double	arc;
+	int		i_res_c;
+	int		n_img;
+	double	ret;
+
+	ret = r_c;
+	arc = r_c / (cyl_c.y * tan(mrt->cone[inter.index].angle / 2));
+	i_res_c = (int)((2 * PI) / arc);
+	n_img = i_res_c / (mrt->cone[inter.index].option.bump_map.width - 1);
+	if (n_img <= 0)
+		ret = (2 * PI) / (mrt->cone[inter.index].option.bump_map.width - 1);
+	else
+	{
+		if (decimal_part((2 * PI) / n_img) <= 0.5)
+			ret = (2 * PI) / (n_img * i_res_c);
+		else
+			ret = (2 * PI) / ((n_img + 1) * i_res_c);
+	}
+	return (ret);
+}
+
+double	get_cone_bdy_res(t_mrt *mrt, t_inter inter, double r_c)
+{
+	int		i_res_h;
+	int		n_img;
+	double	ret;
+
+	ret = r_c;
+	i_res_h = (int)integer_part(mrt->cone[inter.index].height / r_c);
+	n_img = i_res_h / (mrt->cone[inter.index].option.bump_map.height - 1);
+	if (n_img <= 0)
+		ret = mrt->cone[inter.index].height / \
+		(mrt->cone[inter.index].option.bump_map.height - 1);
+	else
+	{
+		if (decimal_part(mrt->cone[inter.index].height / n_img) <= 0.5)
+			ret = mrt->cone[inter.index].height / (n_img * i_res_h);
+		else
+			ret = mrt->cone[inter.index].height / ((n_img + 1) * i_res_h);
+	}
+	return (ret);
+}
+
 t_vec	cone_body_nrml_fr_map(t_mrt *mrt, t_inter inter, t_vec cyl_c, \
 t_c_bump_val values)
 {
-	// int		bump_coor[2];
-	// double	res_circ;
-	// double	res_h;
+	int		bump_coor[2];
+	double	res_circ;
+	double	res_h;
 	t_vec	ret;
 
 	(void)mrt;
@@ -47,17 +92,17 @@ t_c_bump_val values)
 	(void)values;
 
 	ret = fill_coord(0, 0, 1);
-	// res_circ = get_angular_resol(mrt, inter, values.res_cap);
-	// res_h = get_body_resol(mrt, inter, values.res_cap);
-	// bump_coor[1] = (int)integer_part(cyl_c.y / res_circ) % \
-	// mrt->cylinder[inter.index].option.bump_map.width - 1;
-	// bump_coor[0] = (mrt->cylinder[inter.index].option.bump_map.height - 1) \
-	// - (int)integer_part((cyl_c.z + ((mrt->cylinder[inter.index].height) / 2)) \
-	// / res_h) % (mrt->cylinder[inter.index].option.bump_map.height - 1);
-	// pthread_mutex_lock(mrt->mutexs);
-	// ret = bump_nrml_by_coor(&mrt->cylinder[inter.index].option, \
-	// bump_coor[0], bump_coor[1], 0.4);
-	// pthread_mutex_unlock(mrt->mutexs);
+	res_circ = get_cone_ang_resol(mrt, inter, values.res_cap, cyl_c);
+	res_h = get_cone_bdy_res(mrt, inter, values.res_cap);
+	bump_coor[1] = (int)integer_part(cyl_c.y / res_circ) % \
+	mrt->cone[inter.index].option.bump_map.width - 1;
+	bump_coor[0] = (mrt->cone[inter.index].option.bump_map.height - 1) \
+	- (int)integer_part((cyl_c.z + ((mrt->cone[inter.index].height) / 2)) \
+	/ res_h) % (mrt->cone[inter.index].option.bump_map.height - 1);
+	pthread_mutex_lock(mrt->mutexs);
+	ret = bump_nrml_by_coor(&mrt->cone[inter.index].option, \
+	bump_coor[0], bump_coor[1], 0.4);
+	pthread_mutex_unlock(mrt->mutexs);
 	return (ret);
 }
 
@@ -68,7 +113,6 @@ t_vec	cone_nml_frm_map(t_mrt *mrt, t_inter inter, t_vec c_cr, t_vec cyl_cr)
 
 	values.i_diam = i_min_v(mrt->cone[inter.index].option.bump_map.height,
 			mrt->cone[inter.index].option.bump_map.width);
-	// printf("v: %d = min(%d, %d)\n", values.i_diam, mrt->cone[inter.index].option.bump_map.height, mrt->cone[inter.index].option.bump_map.width);
 	values.res_cap = (2 * mrt->cone[inter.index].height * \
 	tan(mrt->cone[inter.index].angle / 2)) / (values.i_diam);
 	ret = fill_coord(0, 0, 1);
@@ -78,4 +122,3 @@ t_vec	cone_nml_frm_map(t_mrt *mrt, t_inter inter, t_vec c_cr, t_vec cyl_cr)
 		ret = cone_body_nrml_fr_map(mrt, inter, cyl_cr, values);
 	return (ret);
 }
-
