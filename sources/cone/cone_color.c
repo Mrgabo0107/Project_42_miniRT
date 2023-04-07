9fc1a969e5238d4e5864281c5d75329403ff19c8
Gabriel Moreno <gdmorenor@unal.edu.co>
2023-04-07 18:44:00 +0200

    fixing a detail

--- sources/cone/cone_color.c
+++ sources/cone/cone_color.c
@@ -6,57 +6,11 @@
 /*   By: gamoreno <gamoreno@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/05 07:27:52 by gamoreno          #+#    #+#             */
-/*   Updated: 2023/04/07 18:29:53 by gamoreno         ###   ########.fr       */
+/*   Updated: 2023/04/07 18:42:19 by gamoreno         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minirt.h"
-
-static double	get_cone_ang_resol(t_mrt *mrt, t_inter inter, \
-double r_c, t_vec cyl_c)
-{
-	double	arc;
-	int		i_res_c;
-	int		n_img;
-	double	ret;
-
-	ret = r_c;
-	arc = r_c / (cyl_c.y * tan(mrt->cone[inter.index].angle / 2));
-	i_res_c = (int)((2 * PI) / arc);
-	n_img = i_res_c / (mrt->cone[inter.index].option.texture.width - 1);
-	if (n_img <= 0)
-		ret = (2 * PI) / (mrt->cone[inter.index].option.texture.width - 1);
-	else
-	{
-		if (decimal_part((2 * PI) / n_img) <= 0.5)
-			ret = (2 * PI) / (n_img * i_res_c);
-		else
-			ret = (2 * PI) / ((n_img + 1) * i_res_c);
-	}
-	return (ret);
-}
-
-static double	get_cone_bdy_res(t_mrt *mrt, t_inter inter, double r_c)
-{
-	int		i_res_h;
-	int		n_img;
-	double	ret;
-
-	ret = r_c;
-	i_res_h = (int)integer_part(mrt->cone[inter.index].height / r_c);
-	n_img = i_res_h / (mrt->cone[inter.index].option.texture.height - 1);
-	if (n_img <= 0)
-		ret = mrt->cone[inter.index].height / \
-		(mrt->cone[inter.index].option.texture.height - 1);
-	else
-	{
-		if (decimal_part(mrt->cone[inter.index].height / n_img) <= 0.5)
-			ret = mrt->cone[inter.index].height / (n_img * i_res_h);
-		else
-			ret = mrt->cone[inter.index].height / ((n_img + 1) * i_res_h);
-	}
-	return (ret);
-}
 
 t_c_chess	get_cone_bdy_col(t_mrt *mrt, int i, t_vec coor, t_rgb color)
 {
--- sources/cone/cone_texture.c
+++ sources/cone/cone_texture.c
@@ -6,11 +6,57 @@
 /*   By: gamoreno <gamoreno@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/05 07:27:52 by gamoreno          #+#    #+#             */
-/*   Updated: 2023/04/07 18:28:26 by gamoreno         ###   ########.fr       */
+/*   Updated: 2023/04/07 18:42:11 by gamoreno         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minirt.h"
+
+static double	get_cone_ang_resol(t_mrt *mrt, t_inter inter, double r_c, \
+t_vec cyl_c)
+{
+	double	arc;
+	int		i_res_c;
+	int		n_img;
+	double	ret;
+
+	ret = r_c;
+	arc = r_c / (cyl_c.y * tan(mrt->cone[inter.index].angle / 2));
+	i_res_c = (int)((2 * PI) / arc);
+	n_img = i_res_c / (mrt->cone[inter.index].option.texture.width - 1);
+	if (n_img <= 0)
+		ret = (2 * PI) / (mrt->cone[inter.index].option.texture.width - 1);
+	else
+	{
+		if (decimal_part((2 * PI) / n_img) <= 0.5)
+			ret = (2 * PI) / (n_img * i_res_c);
+		else
+			ret = (2 * PI) / ((n_img + 1) * i_res_c);
+	}
+	return (ret);
+}
+
+static double	get_cone_bdy_res(t_mrt *mrt, t_inter inter, double r_c)
+{
+	int		i_res_h;
+	int		n_img;
+	double	ret;
+
+	ret = r_c;
+	i_res_h = (int)integer_part(mrt->cone[inter.index].height / r_c);
+	n_img = i_res_h / (mrt->cone[inter.index].option.texture.height - 1);
+	if (n_img <= 0)
+		ret = mrt->cone[inter.index].height / \
+		(mrt->cone[inter.index].option.texture.height - 1);
+	else
+	{
+		if (decimal_part(mrt->cone[inter.index].height / n_img) <= 0.5)
+			ret = mrt->cone[inter.index].height / (n_img * i_res_h);
+		else
+			ret = mrt->cone[inter.index].height / ((n_img + 1) * i_res_h);
+	}
+	return (ret);
+}
 
 t_rgb	cone_cap_txt_fr_map(t_mrt *mrt, t_inter inter, t_vec c_cr, \
 t_c_bump_val values)
